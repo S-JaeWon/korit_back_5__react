65f66ee788c33ffc7d153b076df608af00ad8918
S-JaeWon <ssm5170@gmail.com>
2024-03-21 17:50:38 +0900

    feat: OAuth2

---
+++ library/src/pages/OAuth2SigninPage/OAuth2SigninPage.jsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import * as s from "./style"; 
+
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+function OAuth2SigninPage(props) {
+    const [ searchParams ] = useSearchParams();
+    const accessToken = searchParams.get("accessToken");
+
+    useEffect(() => {
+        localStorage.setItem("AccessToken", accessToken);
+        window.location.replace("/");
+    }, [])
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default OAuth2SigninPage;
---
+++ library/src/pages/OAuth2SigninPage/style.js
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react"
+
+export const container = css`
+    position: relative;
+    box-sizing: border-box;
+    border-radius: 20px;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: #fafafa;
+`;
+
+export const header = css`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    width: 100%;
+`;
